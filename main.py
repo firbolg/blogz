ea34b5023930dfc07ecce0f5d07bbb88af4f9229
Levi Dolan <levijdolan@gmail.com>
2018-04-18 16:09:27 -0500

    created User class and Blogz templates

--- main.py
+++ main.py
@@ -13,12 +13,24 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
    
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
+        self.owner = owner
         
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password, blogs):
+        self.username = username
+        self.password = password
 
 
 
